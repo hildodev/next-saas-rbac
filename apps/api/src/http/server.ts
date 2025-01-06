c7413585b79d21e7ee0b6cfc96af48d6400ae195
Hildebrando Viana Matos <hildocontato@gmail.com>
2025-01-06 13:05:51 +0000

    Rota: Convites pendentes

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -39,7 +39,8 @@ import { getInvite } from '@/http/routes/invites/get-invite'
 import { getInvites } from '@/http/routes/invites/get-invites'
 import { acceptInvite } from '@/http/routes/invites/accept-invite'
 import { rejectInvite } from '@/http/routes/invites/reject-invite'
-import { revokeInvite } from './routes/invites/revoke-invite'
+import { revokeInvite } from '@/http/routes/invites/revoke-invite'
+import { getPendingInvites } from '@/http/routes/invites/get-pending-invites'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -108,6 +109,7 @@ app.register(getInvites)
 app.register(acceptInvite)
 app.register(rejectInvite)
 app.register(revokeInvite)
+app.register(getPendingInvites)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('HTTP server running!')

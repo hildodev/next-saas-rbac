f5ff05a0d3bc284ddd71dfc29cc29d148070fd57
Hildebrando Viana Matos <hildocontato@gmail.com>
2025-01-09 17:51:19 +0000

    Resolvendo problema do tailwindcss

--- apps/web/src/app/auth/forgot-password/page.tsx
+++ apps/web/src/app/auth/forgot-password/page.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
+
 export default function ForgotPasswordPage() {
   return (
     <form action="" className="space-y-4">
--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -2,7 +2,7 @@ import { Button } from '@/components/ui/button'
 
 export default function Home() {
   return (
-    <div>
+    <div className="w-full h-10 bg-red-400">
       <Button>Hello</Button>
     </div>
   )

bc04cab08c653448b666645105cb603a1851dec4
Hildebrando Viana Matos <hildocontato@gmail.com>
2025-01-22 18:13:50 +0000

    Obtendo permissões do usuário: erro no tailwindcss

--- apps/web/src/auth/auth.ts
+++ apps/web/src/auth/auth.ts
@@ -1,14 +1,50 @@
+import { defineAbilityFor } from '@saas/auth'
 import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
 
+import { getMembership } from '@/http/get-membership'
 import { getProfile } from '@/http/get-profile'
 
 export async function isAuthenticated() {
-  return !!(await cookies()).get('token')?.value
+  const cookieStore = await cookies()
+  return !!cookieStore.get('token')?.value
+}
+
+export async function getCurrentOrg() {
+  const cookieStore = await cookies()
+  return cookieStore.get('org')?.value ?? null
+}
+
+export async function getCurrentMembership() {
+  const org = await getCurrentOrg()
+
+  if (!org) {
+    return null
+  }
+
+  const { membership } = await getMembership(org)
+
+  return membership
+}
+
+export async function ability() {
+  const membership = await getCurrentMembership()
+
+  if (!membership) {
+    return null
+  }
+
+  const ability = defineAbilityFor({
+    id: membership.userId,
+    role: membership.role,
+  })
+
+  return ability
 }
 
 export async function auth() {
-  const token = (await cookies()).get('token')?.value
+  const cookieStore = await cookies()
+  const token = cookieStore.get('token')?.value
 
   if (!token) {
     redirect('/auth/sign-in')

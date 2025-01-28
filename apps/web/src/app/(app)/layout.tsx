d67afb99fdf2eaf1c7c6fe605d250b7c19a06f6c
Hildebrando Viana Matos <hildocontato@gmail.com>
2025-01-28 17:26:31 +0000

    Header de volta para as páginaas

--- apps/web/src/app/(app)/layout.tsx
+++ apps/web/src/app/(app)/layout.tsx
@@ -1,6 +1,5 @@
 import { redirect } from 'next/navigation'
 import { isAuthenticated } from '@/auth/auth'
-import { Header } from '@/components/header'
 
 export default function AppLayout({
   children,
@@ -10,10 +9,5 @@ export default function AppLayout({
   if (!isAuthenticated()) {
     redirect('/auth/sign-in')
   }
-  return (
-    <div className="space-y-4 py-4">
-      <Header />
-      <main className="mx-auto w-full max-w-[1200px]">{children}</main>
-    </div>
-  )
+  return <>{children}</>
 }
--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -8,6 +8,7 @@ import { OrganizationSwitcher } from './organization-switcher'
 import { ProfileButton } from './profile-button'
 import { ThemeSwitcher } from './theme/theme-switcher'
 import { Separator } from './ui/separator'
+import Link from 'next/link'
 
 export async function Header() {
   const permissions = await ability()
@@ -15,11 +16,13 @@ export async function Header() {
   return (
     <div className="mx-auto flex max-w-[1200px] items-center justify-between border-b pb-2">
       <div className="flex items-center gap-3">
-        <Image
-          src={rocketseatIcon}
-          className="size-6 dark:invert"
-          alt="Rocketseat"
-        />
+        <Link href="/">
+          <Image
+            src={rocketseatIcon}
+            className="size-6 dark:invert"
+            alt="Rocketseat"
+          />
+        </Link>
 
         <Slash className="size-3 -rotate-[24deg] text-border" />
 

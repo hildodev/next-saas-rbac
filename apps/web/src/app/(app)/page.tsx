f5a65f894c50d11f2b2c05960c2d7ca95138ae2f
Hildebrando Viana Matos <hildocontato@gmail.com>
2025-01-29 13:25:01 +0000

    Interception routes no Next.js

--- apps/web/src/app/(app)/page.tsx
+++ apps/web/src/app/(app)/page.tsx
@@ -1,3 +1,12 @@
+import { Header } from '@/components/header'
+
 export default async function Home() {
-  return <h1>Projects</h1>
+  return (
+    <div className="py-4">
+      <Header />
+      <main className="mx-auto w-full max-w-[1200px] space-y-4">
+        <h1>PROJECTOS</h1>
+      </main>
+    </div>
+  )
 }

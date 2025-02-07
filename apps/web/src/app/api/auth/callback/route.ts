47a9f9fc431cefb795bb691515184922594fc9b6
Hildebrando Viana Matos <hildocontato@gmail.com>
2025-02-07 12:17:02 +0000

    Action: aceitar convite

--- apps/web/src/app/api/auth/callback/route.ts
+++ apps/web/src/app/api/auth/callback/route.ts
@@ -1,6 +1,8 @@
-import { signInWithGithub } from '@/http/sign-in-with-github'
 import { cookies } from 'next/headers'
 import { NextRequest, NextResponse } from 'next/server'
+
+import { acceptInvite } from '@/http/accept-invite'
+import { signInWithGithub } from '@/http/sign-in-with-github'
 
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams
@@ -17,11 +19,19 @@ export async function GET(request: NextRequest) {
   const { token } = await signInWithGithub({ code })
 
   const cookieStore = await cookies()
-
   cookieStore.set('token', token, {
     path: '/',
     maxAge: 60 * 60 * 24 * 7, // 7days
   })
+
+  const inviteId = cookieStore.get('inviteId')?.value
+
+  if (inviteId) {
+    try {
+      await acceptInvite(inviteId)
+      cookieStore.delete('inviteId')
+    } catch {}
+  }
 
   const redirectUrl = request.nextUrl.clone()
 
